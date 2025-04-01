c00b09fd03bf2d28b118a7b643f3f131a451254b
Joe Grigg <joe@ghost.org>
2025-04-01 08:32:49 +0100

    Started moving list view into paginated view to better suit e ink

--- totp/app/index.tsx
+++ totp/app/index.tsx
@@ -4,9 +4,10 @@ import { Header } from '@/components/Header';
 import { Link } from 'expo-router';
 import { getConfig, TotpItemConfig, StorageConfig, defaultConfig } from '@/lib/storage';
 import { useFocusEffect } from 'expo-router';
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useEffect } from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { StatusBar } from 'expo-status-bar'
+import { StatusBar } from 'expo-status-bar';
+import { Button } from '@/components/Button';
 
 const Item: React.FC<{item: TotpItemConfig}> = ({item}) => (
   <Link href={`/code/${item.uuid}`} style={styles.item}>
@@ -19,6 +20,9 @@ const Item: React.FC<{item: TotpItemConfig}> = ({item}) => (
 
 export default function HomeScreen() {
   let [config, setConfig] = useState<StorageConfig>(defaultConfig);
+  let [page, setPage] = useState(0);
+  let [itemsPerPage, _setItemsPerPage] = useState(4);
+  let [items, setItems] = useState<TotpItemConfig[]>([]);
 
   useFocusEffect(
     useCallback(() => {
@@ -30,6 +34,20 @@ export default function HomeScreen() {
     }, [])
   );
 
+  useEffect(() => {
+    // Get items
+    let items = Object.values(config.totps)
+
+    // Pagination
+    items = items.filter((_, i) => i >= (itemsPerPage * page) && i < (itemsPerPage * (page + 1)));
+
+    setItems(items);
+  }, [config.totps, page, itemsPerPage])
+
+  const changePage = (amount: number) => {
+    setPage(Math.max(page+amount));
+  }
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar style="dark" />
@@ -40,19 +58,43 @@ export default function HomeScreen() {
         iconLeft="cog"
         iconLeftLink="/settings"
       />
-      <FlatList
-        data={Object.values(config.totps)}
-        renderItem={({ item }) => <Item item={item} />}
-        keyExtractor={item => item.uuid}
-      />
+      <View style={{flex: 1}}>
+        <FlatList
+          data={items}
+          renderItem={({ item }) => <Item item={item} />}
+          keyExtractor={item => item.uuid}
+          style={styles.list}
+        />
+        <View style={styles.pagination}>
+          <Button
+            icon="chevron-left"
+            iconStyle={{paddingTop: 2, paddingRight: 7}}
+            disabled={page < 1}
+            onPress={() => changePage(-1)}
+          />
+          <Button
+            icon="chevron-right"
+            iconStyle={{paddingTop: 2, paddingLeft: 8}}
+            disabled={page >= Math.floor(Object.keys(config.totps).length / itemsPerPage)}
+            onPress={() => changePage(1)}
+          />
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  list: {
+    margin: 20,
+    marginBottom: 0,
+    borderWidth: 2,
+    borderRadius: 5,
+  },
   item: {
     padding: 20,
-    borderBottomWidth: 2
+    borderBottomWidth: 2,
+    flex: 1
   },
   itemInner: {
     width: '100%',
@@ -62,5 +104,11 @@ const styles = StyleSheet.create({
   },
   itemName: {
     fontSize: 20
+  },
+  pagination: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+    alignItems: 'center'
   }
 });
--- totp/components/Button.tsx
+++ totp/components/Button.tsx
@@ -1,19 +1,29 @@
 import { StyleSheet, TouchableOpacity, Text } from "react-native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export type ButtonProps = {
-    text: string,
+    text?: string,
+    icon?: keyof typeof FontAwesome.glyphMap,
+    iconStyle?: object,
     onPress?: () => void,
     disabled?: boolean
 }
 
 export function Button({
     text,
+    icon,
+    iconStyle,
     onPress,
     disabled
 }: ButtonProps) {
     return (
-        <TouchableOpacity onPress={onPress} disabled={disabled} style={styles.button}>
-            <Text style={styles.buttonText}>{text}</Text>
+        <TouchableOpacity
+            onPress={onPress}
+            disabled={disabled}
+            style={{...styles.button, opacity: disabled ? 0.3 : 1}}
+        >
+            {icon && <FontAwesome name={icon} size={24} style={[styles.buttonIcon, iconStyle]}/>}
+            {text && <Text style={styles.buttonText}>{text}</Text>}
         </TouchableOpacity>
     );
 }
@@ -21,14 +31,20 @@ export function Button({
 const styles = StyleSheet.create({
     button: {
         borderWidth: 2,
-        padding: 5,
-        paddingTop: 10,
-        paddingBottom: 10,
+        padding: 10,
         borderRadius: 5,
-        marginTop: 'auto'
+        marginTop: 'auto',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     buttonText: {
         fontSize: 20,
-        textAlign: 'center'
+        textAlign: 'center',
+    },
+    buttonIcon: {
+        textAlign: 'center',
+        paddingLeft: 5,
+        paddingRight: 5
     }
 });
